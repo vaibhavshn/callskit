ae11feecfac59885980aec2f3705b8ce4b0cdb07
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2025-03-30 13:57:53 +0530

    feat: browser iife build

--- package/src/vite-env.d.ts
+++ package/src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	// more env variables...
+	// env variables
 }
 
 interface ImportMeta {
---
+++ package/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vite.dev/config/
+export default defineConfig({
+	build: {
+		emptyOutDir: false,
+		lib: {
+			entry: {
+				browser: 'src/index.ts',
+			},
+			fileName: () => 'browser.js',
+			name: 'CallsKit',
+			formats: ['iife'],
+		},
+	},
+	plugins: [react()],
+});
