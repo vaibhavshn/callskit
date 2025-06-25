c4d66ca345117f9873d353599c6a1c553e23e0b2
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2025-06-25 10:34:40 +0530

    fix(server): use Map instead of array

--- package/src/server/index.ts
+++ package/src/server/index.ts
@@ -6,18 +6,16 @@ import type { ChatMessage } from '..';
 export class PartyKitServer implements Party.Server {
 	started_at!: Date;
 
-	users: User[] = [];
+	users = new Map<string, User>();
 
 	chat: ChatMessage[] = [];
-
-	connectionIds = new Set<string>();
 
 	constructor(readonly room: Party.Room) {}
 
 	async onStart() {
-		this.clearUsers();
 		this.started_at = new Date();
-		this.users = (await this.room.storage.get<User[]>('users')) ?? [];
+		const storedUsers = (await this.room.storage.get<User[]>('users')) ?? [];
+		this.users = new Map(storedUsers.map((user) => [user.connectionId, user]));
 		this.chat = (await this.room.storage.get<ChatMessage[]>('chat')) ?? [];
 	}
 
@@ -28,50 +26,42 @@ export class PartyKitServer implements Party.Server {
   room: ${this.room.id}
   url: ${new URL(ctx.request.url).pathname}`,
 		);
-		this.connectionIds.add(conn.id);
 		conn.send(createEvent({ event: 'connected' }));
 	}
 
-	onClose(connection: Party.Connection): void | Promise<void> {
-		this.connectionIds.delete(connection.id);
-		const user = this.getUser(connection.id);
+	async onClose(connection: Party.Connection) {
+		const user = this.users.get(connection.id);
 		if (user) {
-			this.removeUser(connection.id);
+			this.users.delete(connection.id);
 			this.room.broadcast(
 				createEvent({ event: 'participant/left', participantId: user.id }),
 			);
 		}
 
-		if (this.users.length === 0) {
-			this.room.storage.put<ChatMessage[]>('chat', this.chat);
-			setTimeout(() => {
-				if (this.users.length === 0) {
-					this.room.storage.delete('users');
-					this.room.storage.delete('chat');
-				}
-			}, 1000 * 60);
+		if (this.users.size === 0) {
+			await this.room.storage.deleteAll();
 		}
 	}
 
 	onMessage(message: string, sender: Party.Connection) {
-		console.log(`connection ${sender.id} sent message: ${message}`);
 		const payload = parseAction(message);
+		const user = this.users.get(sender.id);
+		
+		console.log(`received message from ${sender.id}: ${payload}`);
 
 		switch (payload.action) {
 			case 'join': {
 				const user: User = { ...payload.self, connectionId: sender.id };
-				const without = this.getConnectionIds([sender.id]);
-				console.log('join', without, sender.id);
 				this.room.broadcast(
 					createEvent({
 						event: 'room/init',
-						participants: this.users,
+						participants: [...this.users.values()],
 						started_at: this.started_at.toISOString(),
 						chatMessages: this.chat,
 					}),
-					without,
+					[...this.users.keys()],
 				);
-				this.addUser(user);
+				this.users.set(sender.id, user);
 				this.room.broadcast(
 					createEvent({
 						event: 'participant/joined',
@@ -79,13 +69,12 @@ export class PartyKitServer implements Party.Server {
 					}),
 					[sender.id],
 				);
-				console.log(this.users);
 				break;
 			}
 
 			case 'leave': {
-				const user = this.getUser(sender.id);
 				if (user) {
+					this.users.delete(sender.id);
 					this.room.broadcast(
 						createEvent({
 							event: 'participant/left',
@@ -94,16 +83,12 @@ export class PartyKitServer implements Party.Server {
 						[sender.id],
 					);
 				}
-				this.removeUser(sender.id);
 				break;
 			}
 
 			case 'self/mic-update': {
-				const { micEnabled, micTrackId } = payload.updates;
-				const user = this.getUser(sender.id);
 				if (user) {
-					user.micEnabled = micEnabled;
-					user.micTrackId = micTrackId;
+					Object.assign(user, payload.updates);
 					this.room.broadcast(
 						createEvent({
 							event: 'participant/mic-update',
@@ -116,11 +101,8 @@ export class PartyKitServer implements Party.Server {
 			}
 
 			case 'self/camera-update': {
-				const { cameraEnabled, cameraTrackId } = payload.updates;
-				const user = this.getUser(sender.id);
 				if (user) {
-					user.cameraEnabled = cameraEnabled;
-					user.cameraTrackId = cameraTrackId;
+					Object.assign(user, payload.updates);
 					this.room.broadcast(
 						createEvent({
 							event: 'participant/camera-update',
@@ -133,20 +115,8 @@ export class PartyKitServer implements Party.Server {
 			}
 
 			case 'self/screenshare-update': {
-				const {
-					screenshareEnabled,
-					screenshareVideoTrackId,
-					screenshareAudioTrackId,
-				} = payload.updates;
-				const user = this.getUser(sender.id);
 				if (user) {
-					user.screenshareEnabled = screenshareEnabled;
-					if (screenshareVideoTrackId) {
-						user.screenshareVideoTrackId = screenshareVideoTrackId;
-					}
-					if (screenshareAudioTrackId) {
-						user.screenshareAudioTrackId = screenshareAudioTrackId;
-					}
+					Object.assign(user, payload.updates);
 					this.room.broadcast(
 						createEvent({
 							event: 'participant/screenshare-update',
@@ -159,9 +129,8 @@ export class PartyKitServer implements Party.Server {
 			}
 
 			case 'chat/message': {
-				const user = this.getUser(sender.id);
 				if (user) {
-					const message = {
+					const message: ChatMessage = {
 						...payload.message,
 						id: crypto.randomUUID(),
 						display_name: user.name,
@@ -176,49 +145,6 @@ export class PartyKitServer implements Party.Server {
 				break;
 			}
 		}
-	}
-
-	getUser(connectionId: string) {
-		return this.users.find((u) => u.connectionId === connectionId);
-	}
-
-	async addUser(user: User) {
-		console.log(`addUser:${user.id}:${user.name}`);
-		this.users.push(user);
-		this.connectionIds.delete(user.connectionId);
-		return this.room.storage.put<User[]>('users', this.users);
-	}
-
-	async removeUser(connectionId: string) {
-		const user = this.getUser(connectionId);
-
-		if (user) {
-			console.log(`removeUser:${user.id}:${user.name}`);
-			this.users = this.users.filter(
-				(user) => user.connectionId !== connectionId,
-			);
-			return this.syncUsersToDB();
-		}
-	}
-
-	async syncUsersToDB() {
-		await this.room.storage.put<User[]>('users', this.users);
-	}
-
-	async clearUsers() {
-		this.users = [];
-		await this.room.storage.delete('users');
-	}
-
-	getConnectionIds(exclude?: string[]) {
-		const ids = [
-			...this.users.map((u) => u.connectionId),
-			...this.connectionIds.values(),
-		];
-		if (exclude) {
-			return ids.filter((id) => !exclude.includes(id));
-		}
-		return ids;
 	}
 }
 
