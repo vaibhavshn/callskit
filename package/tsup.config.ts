9cd8b4173cc29c25cf426cd7b5678523d153ac5c
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2025-08-19 19:00:06 +0530

    chore: upgrade server deps

--- package/tsup.config.ts
+++ package/tsup.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'tsup';
 
 const isProd = process.env.NODE_ENV === 'production';
 
+if (isProd) {
+	console.log('\x1b[1m\x1b[32m%s\x1b[0m', 'Building for production...\n');
+} else {
+	console.log(
+		'\x1b[1m\x1b[33m%s\x1b[0m',
+		'Starting watch build for development...\n',
+	);
+}
+
 export default defineConfig([
 	{
 		entry: {
